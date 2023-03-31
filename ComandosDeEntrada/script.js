9b1cc939a2cee7992ce074e40a77731ddb7e38da
David Henrique Silva dos Santos <davidhenriquehy@gmail.com>
2023-03-31 19:06:08 -0300

    js aula 1

---
+++ ComandosDeEntrada/script.js
@@ -0,0 +1,11 @@
+var nome = prompt('Qual seu nome');
+
+var sobreNome = prompt('Qual seu sobrenome?');
+
+console.log('ola ' + nome + ' ' + sobreNome);
+
+/* document.write("<h1> Bem vindo " + nome + " " + sobreNome + " " + "</h1>") */
+
+document.write(`<h1> BEM VINDO ${nome} ${sobreNome} </h1>`)
+
+document.write("<img src='https://img.estadao.com.br/resources/jpg/4/6/1552489457364.jpg' a/>")
